4a1586b281b52b052c5a74d38d4c6713c5e5fe3f
Antoine Reyt <antoine.reyt@gmail.com>
2015-03-04 01:15:41 +0100

    Added debug infos

--- master/src/calcul_master.erl
+++ master/src/calcul_master.erl
@@ -1,7 +1,7 @@
 -module(calcul_master).
 -behaviour(application).
 -export([start/2, stop/1]).
--export([sum_squares/2]).
+-export([calculate/2]).
 
 start(normal, []) ->
     calcul_master_sup:start_link().
@@ -9,26 +9,43 @@ start(normal, []) ->
 stop(_State) ->
     ok.
 
-sum_squares(N, M) ->
+calculate(N, M) ->
   Ranges = calcul_master_range:split(N, M, length(nodes())),
   Params = lists:zip(Ranges, nodes()),
 
-  MapperPids = [ spawn_job(Param) || Param <- Params ],
+  % Map
+  WorkerPids = [ spawn_jobs(Param) || Param <- Params ],
+  Results    = [ wait_responses(Pid) || Pid <- WorkerPids ],
 
-  Results = [ spawn_receive(Pid) || Pid <- MapperPids ],
+  % Reduce
+  Result = lists:foldl(
+    fun(ResultInfo, Sum) ->
+      {_, _, Result} = ResultInfo,
+      Result + Sum
+    end, 0, Results),
 
-  lists:foldl(fun(ResultInfo, Sum) -> {_, _, Result} = ResultInfo, Result + Sum end, 0, Results).
+  % Display results and times
+  display_results_table(Results, Result),
+  Result.
 
-spawn_job({{N, M}, Node}) ->
+display_results_table(Results, FinalResult) ->
+  % Lines
+  lists:map(fun(R) ->
+    {Node, Time, Result} = R,
+    io:format("~n~p | ~pms | ~p", [Node, Time / 1000, Result])
+    end, Results),
+  % FinalResult
+  io:format("~n~nResult: ~p~n~n", [FinalResult]).
+
+spawn_jobs({{N, M}, Node}) ->
   Answer_to = self(),
   spawn(fun () ->
     {Time, Result} = timer:tc(gen_server, call, [{calcul_slave_server, Node}, {calculate, N, M}]),
     Answer_to ! {self(), Node, Time, Result}
   end).
 
-spawn_receive(Pid) ->
+wait_responses(Pid) ->
   receive
     {Pid, Node, Time, Result} ->
       {Node, Time, Result}
   end.
-
